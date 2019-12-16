3a736020d8b1abe736213dd287407e1b6915f2d4
tanzy1998 <44250283+tanzy1998@users.noreply.github.com>
2019-12-16 16:56:48 +0800

    Update P6.cpp

--- P6/P6/P6.cpp
+++ P6/P6/P6.cpp
@@ -8,12 +8,19 @@
 #pragma comment (lib, "OpenGL32.lib")
 #pragma comment (lib, "GLU32.lib")
 
-#define WINDOW_TITLE "Practical 5"
+#define WINDOW_TITLE "Practical 6"
 
-float rotatedeg = 0.0;
-float x = 0, y = 0, z = 0;
-float amb[] = { 1,1,1 };
-float pos[] = { x,y,z };
+int rotate = 0;
+bool pyramid = false;
+bool light = true;
+
+float diffuseColor[] = { 1,1,1 };
+float diffusePosition[] = { 0.0f, 0.5f, 0.0f };
+float objectColor[] = { 0,0,0 };
+
+double w = 1920;
+double h = 1080;
+double ar = w / h; // aspect ratio
 
 LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -22,23 +29,30 @@ LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam
 	case WM_CHAR:
 		switch (wParam)
 		{
-		case 0x57:
-			y += 10.0;
+		case 0x44:
+			diffusePosition[0] += 0.3;
 			break;
 		case 0x41:
-			x -= 10.0;
+			diffusePosition[0] -= 0.3;
+			break;
+		case 0x57:
+			diffusePosition[1] += 0.3;
 			break;
 		case 0x53:
-			y -= 10.0;
+			diffusePosition[1] -= 0.3;
 			break;
-		case 0x44:
-			x += 10.0;
-			break;
+		
 		case 0x45:
-			z += 10.0;
+			diffusePosition[2] += 0.3;
 			break;
 		case 0x51:
-			z -= 10.0;
+			diffusePosition[2] -= 0.3;
+			break;
+		case 0x4F:
+			pyramid = false;
+			break;
+		case 0x50:
+			pyramid = true;
 			break;
 		}
 		break;
@@ -53,7 +67,24 @@ LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam
 		case VK_ESCAPE:
 			PostQuitMessage(0);
 			break;
-
+		case VK_SPACE:
+			if (light)
+				light = false;
+			else
+				light = true;
+			break;
+		case VK_UP:
+			if (rotate == 0)
+				rotate = 1;
+			else
+				rotate = 0;
+			break;
+		case VK_DOWN:
+			if (rotate == 0)
+				rotate = 2;
+			else
+				rotate = 0;
+			break;
 		}
 		break;
 	default:
@@ -95,49 +126,50 @@ bool initPixelFormat(HDC hdc)
 	}
 }
 
-void pyramid()
+void drawPyramid()
 {
 	glPushMatrix();
 	glColor3ub(180, 255, 145);
-	glTranslatef(0, -0.5f, 0);
+	glTranslatef(0, -0.3f, 0);
 	//glRotatef(rotateDeg, 1, 1, 1);
 	glBegin(GL_POLYGON);
 
 	glColor3f(0, 1, 0);
-	glVertex3f(0, 1, -0.5);
-	glVertex3f(-0.5, 0, 0);
-	glVertex3f(0.5, 0, 0);
+	glVertex3f(0, 0.5, -0.3);
+	glVertex3f(-0.3, 0, 0);
+	glVertex3f(0.3, 0, 0);
 
 	glColor3f(0, 1, 0);
-	glVertex3f(0, 1, -0.5);
-	glVertex3f(-0.5, 0, 0);
-	glVertex3f(-0.5, 0, -1.0);
+	glVertex3f(0, 0.5, -0.3);
+	glVertex3f(-0.3, 0, 0);
+	glVertex3f(-0.3, 0, -1.0);
 
 	glColor3f(0, 1, 0);
-	glVertex3f(0, 1, -0.5);
-	glVertex3f(0.5, 0, 0);
-	glVertex3f(0.5, 0, -1.0);
+	glVertex3f(0, 0.5, -0.3);
+	glVertex3f(0.3, 0, 0);
+	glVertex3f(0.3, 0, -1.0);
 
 	glColor3f(0, 1, 0);
-	glVertex3f(0, 1, -0.5);
-	glVertex3f(0.5, 0, -1.0);
-	glVertex3f(-0.5, 0, -1.0);
+	glVertex3f(0, 0.5, -0.3);
+	glVertex3f(0.3, 0, -1.0);
+	glVertex3f(-0.3, 0, -1.0);
 
 	glColor3f(0, 1, 0);
-	glVertex3f(-0.5, 0, 0);
-	glVertex3f(0.5, 0, 0);
-	glVertex3f(0.5, 0, -1.0);
-	glVertex3f(-0.5, 0, -1.0);
+	glVertex3f(-0.3, 0, 0);
+	glVertex3f(0.3, 0, 0);
+	glVertex3f(0.3, 0, -1.0);
+	glVertex3f(-0.3, 0, -1.0);
 
 	glEnd();
 	glPopMatrix();
 }
 
-void sphere() {
+void drawSphere() {
 	GLUquadricObj* sphere = NULL;
 	sphere = gluNewQuadric();
+	glMaterialfv(GL_FRONT, GL_AMBIENT, objectColor);
 	gluQuadricDrawStyle(sphere, GLU_LINE);
-	gluSphere(sphere, 0.5, 30, 30);
+	gluSphere(sphere, 0.3, 30, 30);
 	gluDeleteQuadric(sphere);
 }
 
@@ -145,17 +177,55 @@ void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glEnable(GL_DEPTH_TEST);
-
-	glEnable(GL_LIGHTING);
-	//glLightfv(GL_LIGHT0, GL_AMBIENT, amb);
-	glLightfv(GL_LIGHT0, GL_POSITION, pos);
-	glEnable(GL_LIGHT0);
-
-	sphere();
+	glClearColor(1, 1, 1, 1);
+	if (light)
+	{
+		glPushMatrix();
+		{
+			glLoadIdentity();
+			glEnable(GL_LIGHTING);
+			glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseColor);
+			glLightfv(GL_LIGHT0, GL_POSITION, diffusePosition);
+			glEnable(GL_LIGHT0);
+		}
+		glPopMatrix();
+	}
 	
-	//sphere();
+	if (rotate == 1)
+	{
+		glRotatef(-0.5, 1, 1, 1);
+	}
+	else if (rotate == 2)
+	{
+		glRotatef(0.5, 1, 1, 1);
+	}
+
+	if (!pyramid)
+	{
+		drawSphere();
+	}
+	else
+	{
+		drawPyramid();
+	}
 	
+	glPushMatrix();
+	{
+		glLoadIdentity();
+		
+		glDisable(GL_LIGHT0);
+		GLUquadricObj* sphere = NULL;
+		glMaterialfv(GL_FRONT, GL_AMBIENT, objectColor);
+		sphere = gluNewQuadric();
+		glTranslatef(diffusePosition[0], diffusePosition[1], diffusePosition[2]);
+		gluQuadricDrawStyle(sphere, GLU_FILL);
+		gluSphere(sphere, 0.05, 50, 50);
+		gluDeleteQuadric(sphere);
+	}
+	glPopMatrix();
 }
+
+
 //--------------------------------------------------------------------
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevInstance,
